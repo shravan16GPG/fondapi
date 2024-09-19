b69138b281ad16c426ea7953396a8833a6fe10ae
Ashackq <>
2024-09-19 20:08:27 +0530

    gitignore

--- api.js
+++ api.js
@@ -14,15 +14,19 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.enable("trust proxy");
-// const allowedOrigins = ["https://fond-46ce4.web.app/", "http://localhost:1000"];
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: "http://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "This is your API response" });
+  if (req.body.username === "Akash") {
+    res.json({ message: "You are a superuser" });
+  } else {
+    res.json({ message: "This is your API response" });
+  }
   console.log(req.cookies.jwt);
 });
 
@@ -30,5 +34,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/canteens", canteenroutes);
 app.use("/api/v1/items", itemroutes);
 app.use("/api/v1/orders", orderingRouter);
+app.use("/api/v1/bills");
+app.use("/api/v1/timetable");
 
 module.exports = app;
